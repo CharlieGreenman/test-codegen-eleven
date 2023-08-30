d9af533b94ca04a7071d1e936a0c153998b28e41
Charlie Greenman <charlie@razroo.com>
2023-08-30 01:18:15 -0700

    Add code for angular-ai-16.2.0 angular-auth-0-service-angular-service

---
+++ libs/angular-auth-0-service/angular-auth-0-service.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AngularServiceService {
+
+  constructor() { }
+
+  // Add your service methods here
+
+}
